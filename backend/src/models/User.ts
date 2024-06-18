d665d7571c023fb12984ee1037cc4be769572a41
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2024-06-19 02:21:50 +0500

    Lecture 3: Defining models and associations ( Users, categories, posts, comments, tags, post_tags, tokens )

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,10 +1,36 @@
-import { Column, Model, Table } from "sequelize-typescript";
+import { Column, HasMany, Model, Table } from "sequelize-typescript";
+import { Post } from "./Post";
+import { Comment } from "./Comment";
+import { Token } from "./Token";
 
 @Table
 export class User extends Model<User> {
 
 
-  @Column
+  @Column({
+    allowNull: false
+  })
   name: string = "";
+
+  @Column({
+    unique: true,
+    allowNull: false
+  })
+  email: string = "";
+
+  @Column({
+    allowNull: false
+  })
+  password: string = "";
+
+
+  @HasMany(()=>Post)
+  posts: Post[] = []
+
+  @HasMany(()=>Comment)
+  comments: Comment[] = []
+
+  @HasMany(()=>Token)
+  tokens: Token[] = []
+
 }
- 
