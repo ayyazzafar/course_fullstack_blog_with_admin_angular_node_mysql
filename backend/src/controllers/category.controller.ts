728aac68d03f07449bb7f8e806ac596c05736f77
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2024-06-20 10:07:32 +0500

    Lecture 4: Creating basic API Structure

---
+++ backend/src/controllers/category.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { getAllCategories } from "../services/category.service";
+
+
+
+export const getCategories = async (req: Request, res: Response) =>{
+
+    const categories = await getAllCategories();
+
+    res.json(categories)
+}
