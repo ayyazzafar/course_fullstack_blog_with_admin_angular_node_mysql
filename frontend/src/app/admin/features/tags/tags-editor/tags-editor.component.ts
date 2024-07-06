4bd189641bb9d218b74faaf156ac1043c1fbfb27
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2024-07-06 18:51:04 +0500

    Frontend Development > Creating Basic folder / project structure

---
+++ frontend/src/app/admin/features/tags/tags-editor/tags-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tags-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './tags-editor.component.html',
+  styleUrl: './tags-editor.component.scss'
+})
+export class TagsEditorComponent {
+
+}
